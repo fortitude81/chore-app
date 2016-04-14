f3595d76b91f790715a0e8346f9e97e51e55ffc9
Justin Bennett <fortitude1981@gmail.com>
2016-04-14 14:29:58 -0500

    more updates

--- src/server/db/db.js
+++ src/server/db/db.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/todos');
-//mongoose.connect('mongodb://jb:123@ds021000.mlab.com:21000/choreapp');
+//mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/todos');
+mongoose.connect('mongodb://jb:123@ds011860.mlab.com:11860/choreapp');
+
 var Todo = mongoose.model('Todo', {
     task: String,
     isCompleted: Boolean,
